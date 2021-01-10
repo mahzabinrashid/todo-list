1b85182a5caa291b9d0a6613d3dec0014c564398
Fariha <mahzabin741@gmail.com>
2021-01-11 02:46:00 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect
+import os
+
+app = Flask(__name__)
+
+todos = []
+
+@app.route('/')
+def tasks():
+    return render_template("tasks.html", todos=todos)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "GET":
+        return render_template("add.html")
+    else:
+        todo = request.form.get("task")
+        todos.append(todo)
+        return redirect("/")
+
+
+if __name__ == "__main__":    
+    app.run() 
+
+        
